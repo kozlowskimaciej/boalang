bfa531bee26932ba789bb667ea0f0f881c452ee4
mkozlo13 <maciejmaciejkoz@gmail.com>
2024-03-23 21:58:53 +0100

    Add FileSource to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,14 @@
 #include "lexer/lexer.hpp"
 #include "source/source.hpp"
 
-int main() {
-  auto src = StringSource(L",.{}\"Hello, World!\" hi // a comment\n hello 0.01 123 2147483648");
-  auto lexer = Lexer(src);
+int main(int argc, char *argv[]) {
+  Source* src;
+  if (argc == 2) {
+    src = new FileSource(argv[1]);
+  } else {
+    src = new StringSource(L",.{}\"Hello, World!\" hi // a comment\n hello 0.01 123 2147483648");
+  }
+  auto lexer = Lexer(*src);
   while (true) {
     auto token = lexer.next_token();
     std::wcout << token << L' ';
