bbf0f62540a1647abe91c0389435e80ee3c14b93
mkozlo13 <01169196@pw.edu.pl>
2024-05-29 21:03:38 +0200

    Add Scope

---
+++ src/interpreter/scope/scope.cpp
@@ -0,0 +1,113 @@
+#include "scope.hpp"
+
+#include <algorithm>
+
+template <class... Ts>
+struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
+
+bool Scope::type_in_variant(std::vector<VarType> variant_types, BuiltinType type) const {
+  return std::ranges::any_of(variant_types, [&type](const VarType& param) {
+    return param.type == type;
+  });
+}
+
+bool Scope::identifier_in_variant(std::vector<VarType> variant_types, std::string identifier) const {
+  if (!get_type(identifier)) {
+    return false;
+  }
+
+  return std::ranges::any_of(variant_types, [&identifier](const VarType& param) {
+    return param.type == IDENTIFIER && param.name == identifier;
+  });
+}
+
+bool Scope::match_type(const eval_value_t& actual, const VarType& expected) const {
+  if (!expected.name.empty() && expected.type == IDENTIFIER) {
+    if (const auto& type = get_type(expected.name)) {
+      if (const auto& variant = std::get_if<std::shared_ptr<VariantType>>(&*type)) {
+        return std::visit(overloaded{
+            [&](const value_t& value) {
+              return std::visit(overloaded{
+                  [&](std::monostate) { return type_in_variant(variant->get()->types, VOID); },
+                  [&](const std::string &) { return type_in_variant(variant->get()->types, STR); },
+                  [&](int) { return type_in_variant(variant->get()->types, INT); },
+                  [&](float) { return type_in_variant(variant->get()->types, FLOAT); },
+                  [&](bool) { return type_in_variant(variant->get()->types, BOOL); },
+              }, value);
+            },
+            [&](const std::shared_ptr<Variable>& obj) { return identifier_in_variant(variant->get()->types, obj->type.name); },
+            [&](const std::shared_ptr<StructObject>& obj) { return identifier_in_variant(variant->get()->types, obj->type_def->type_name); },
+            [&](const std::shared_ptr<VariantObject>& obj) { return identifier_in_variant(variant->get()->types, obj->type_def->type_name); },
+        }, actual);
+      }
+    }
+  }
+
+  return std::visit(overloaded{
+      [&expected](const value_t& value) {
+        return std::visit(overloaded{
+            [&expected](std::monostate) { return expected.type == VOID; },
+            [&expected](const std::string &) { return expected.type == STR; },
+            [&expected](int) { return expected.type == INT; },
+            [&expected](float) { return expected.type == FLOAT; },
+            [&expected](bool) { return expected.type == BOOL; },
+        }, value);
+      },
+        [&expected](const std::shared_ptr<Variable>& obj) { return expected.type == IDENTIFIER && expected.name == obj->name; },
+        [&expected](const std::shared_ptr<StructObject>& obj) { return expected.type == IDENTIFIER && expected.name == obj->name; },
+        [&expected](const std::shared_ptr<VariantObject>& obj) { return expected.type == IDENTIFIER && expected.name == obj->name; },
+      }, actual);
+}
+
+void Scope::define(const std::string& name, var_t variable) {
+  variables.insert({name, std::move(variable)});
+}
+
+void Scope::assign(const std::string& name, eval_value_t new_value) {
+  auto entry = variables.find(name);
+  if (entry != variables.end()) {
+    auto& item = entry->second;
+    if (const auto &variable = std::get_if<std::shared_ptr<Variable>>(&item)) {
+      (*variable)->value = std::move(new_value);
+    }
+    return;
+  }
+
+  if (enclosing != nullptr) {
+    enclosing->assign(name, std::move(new_value));
+    return;
+  }
+
+  throw RuntimeError(name + " not found in scope");
+}
+
+std::optional<var_t> Scope::get(const std::string& name) const {
+  auto item = variables.find(name);
+  if (item != variables.end()) {
+    return item->second;
+  }
+  if (enclosing != nullptr) {
+    return enclosing->get(name);
+  }
+  return std::nullopt;
+}
+
+std::optional<types_t> Scope::get_type(const std::string &name) const {
+  auto item = types.find(name);
+  if (item != types.end()) {
+    return item->second;
+  }
+  if (enclosing != nullptr) {
+    return enclosing->get_type(name);
+  }
+  return std::nullopt;
+}
+
+void Scope::define_type(const std::string &name, types_t type) {
+  types.insert({name, std::move(type)});
+}
---
+++ src/interpreter/scope/scope.hpp
@@ -0,0 +1,98 @@
+/*! @file scope.hpp
+    @brief boalang interpreter's scope.
+*/
+
+#ifndef BOALANG_SCOPE_HPP
+#define BOALANG_SCOPE_HPP
+
+#include <string>
+#include <map>
+#include <memory>
+#include <optional>
+#include <variant>
+#include <vector>
+
+#include "utils/errors.hpp"
+#include "token/token.hpp"
+
+// forward declarations
+struct Variable;
+struct StructObject;
+struct VariantObject;
+//struct FunctionObject;
+class Scope;
+
+using eval_value_t = std::variant<value_t, std::shared_ptr<StructObject>, std::shared_ptr<VariantObject>, std::shared_ptr<Variable>>;  // , FunctionObject
+using var_t = std::variant<std::shared_ptr<Variable>, std::shared_ptr<StructObject>, std::shared_ptr<VariantObject>>;
+
+struct Variable {
+  VarType type;
+  std::string name;
+  bool mut;
+  std::optional<eval_value_t> value;
+
+  Variable(VarType type, std::string name, bool mut, eval_value_t value)
+      : type(std::move(type)), name(std::move(name)), mut(mut), value(std::move(value)) {}
+  Variable(VarType type, std::string name, bool mut)
+      : type(std::move(type)), name(std::move(name)), mut(mut), value(std::nullopt) {}
+};
+
+struct StructType {
+  std::string type_name;
+  std::map<std::string, Variable> init_fields;
+
+  StructType(std::string type_name, std::map<std::string, Variable> init_fields) : type_name(std::move(type_name)), init_fields(std::move(init_fields)) {};
+};
+
+struct StructObject {
+  StructType* type_def;
+  std::string name;
+  std::unique_ptr<Scope> scope;
+
+  StructObject(StructType* type_def, std::string name, std::unique_ptr<Scope> scope) :
+      type_def(type_def), name(std::move(name)), scope(std::move(scope)) {};
+};
+
+struct VariantType {
+  std::string type_name;
+  std::vector<VarType> types;
+
+  VariantType(std::string type_name, std::vector<VarType> types) : type_name(std::move(type_name)), types(std::move(types)) {};
+};
+
+struct VariantObject {
+  VariantType* type_def;
+  bool mut;
+  std::string name;
+  eval_value_t contained;
+
+  VariantObject(VariantType* type_def, bool mut, std::string name, eval_value_t contained) :
+      type_def(type_def), mut(mut), name(std::move(name)), contained(std::move(contained)) {};
+};
+
+//struct FunctionObject {
+//
+//};
+
+using types_t = std::variant<std::shared_ptr<StructType>, std::shared_ptr<VariantType>>;
+
+class Scope {
+  std::map<std::string, var_t> variables{};
+  std::map<std::string, types_t> types{};
+  Scope* enclosing;
+
+ public:
+  Scope() : enclosing(nullptr) {};
+  Scope(Scope* enclosing) : enclosing(enclosing) {};
+
+  void define(const std::string& name, var_t variable);
+  void define_type(const std::string& name, types_t type);
+  void assign(const std::string& name, eval_value_t new_value);
+  [[nodiscard]] std::optional<var_t> get(const std::string& name) const;
+  [[nodiscard]] std::optional<types_t> get_type(const std::string& name) const;
+  [[nodiscard]] bool match_type(const eval_value_t& actual, const VarType& expected) const;
+  [[nodiscard]] bool type_in_variant(std::vector<VarType> variant_types, BuiltinType type) const;
+  [[nodiscard]] bool identifier_in_variant(std::vector<VarType> variant_types, std::string identifier) const;
+};
+
+#endif  // BOALANG_SCOPE_HPP
